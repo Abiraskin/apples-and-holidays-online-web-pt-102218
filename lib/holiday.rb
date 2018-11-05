2df55b74ac95ff6bed70dbb44221aab204f24e8d
Abi Raskin <abiraskin@gmail.com>
2018-11-05 02:46:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,10 +50,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].collect do |holiday, supplies|
+  holiday_hash[:winter].map do |holiday, supplies|
     supplies
-  end
-    supplies.flatten
+  end.flatten
 
 end
 
