0ef8f6196a5822488ce2c64d05ceacf75e19b21d
Abi Raskin <abiraskin@gmail.com>
2018-11-05 03:10:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
-      holiday if supplies.include? "BBQ"
+      holiday if supplies.include?("BBQ")
     end
   end
 
